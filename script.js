ee5af4d247c3d7e782bdc28febd0264fe9a8bbbc
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-03-25 11:47:57 -0500

    comment

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
 //this is the JS file that will house the backend for the APIs
-
 const today = new Date();
 const day = today.getDate();
 const month = today.getMonth()+1;
 const year = today.getFullYear();
 
-// the follow two lines are for changing the html, need update to change the css. may require playing with it. can you call css from the js?
-const filename = `page-${year}-${month}-${day}.html`;
-window.location.href = filename;
+//the following is copied from freeCodeCamp. will need to tweak it for my purposes. 
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('sendMessage').onclick = function () {
+        const userName = document.getElementById('name').value;
+        const url = 'https://jsonplaceholder.typicode.com/posts';
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 201) {
+                const serverResponse = JSON.parse(xhr.response);
+                document.getElementsByClassName('message')[0].textContent = serverResponse.userName + serverResponse.suffix;
+            };
+        };
+    const body = JSON.stringify({ userName: userName, suffix: ' loves cats!' });
+    xhr.send(body);
+    };
+});
+*/
+/*
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    document.getElementById('data').innerHTML =
+      'latitude: ' +
+      position.coords.latitude +
+      '<br>longitude: ' +
+      position.coords.longitude;
+  });
+}
+*/
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('getMessage').onclick = function () {
+        const req = new XMLHttpRequest();
+        req.open('GET', '/json/cats.json', true);
+        req.send();
+        req.onload = function () {
+            let json = JSON.parse(req.responseText);
+            let html = '';
+            json = json.filter(function (val) {
+                return val.id !== 1;
+            });
+            json.forEach(function (val) {
+                html += "<div class = 'cat'>";
+                html +="<img src='" + val.imageLink + "' " + "alt='" + val.altText + "'>";
+                html += '</div>';
+            });
+            document.getElementsByClassName('message')[0].innerHTML = html;
+        };
+    };
+});
+*/
